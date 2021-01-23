78afaccd32d189da196bf2b3cded10237015aa32
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-01-23 11:28:12 -0300

    aula 17 - correcao tipo do projeto

--- ProAgil.WebAPI/Controllers/ValuesController.cs
+++ ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ProAgil.API.Controllers
+namespace ProAgil.WebAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("site/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace ProAgil.API
+namespace ProAgil.WebAPI
 {
     public class Startup
     {
@@ -41,7 +41,7 @@ namespace ProAgil.API
                 app.UseHsts();
             }
 
-            //app.UseHttpsRedirection();
+          //  app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
