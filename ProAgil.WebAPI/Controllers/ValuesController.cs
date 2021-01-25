86b3c42483e9006337b01b1d010a04a1a3b1c70b
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-01-24 23:00:44 -0300

    Aula 23 - TryCatch e Status Code

--- ProAgil.WebAPI/Controllers/ValuesController.cs
+++ ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProAgil.WebAPI.Data;
 using ProAgil.WebAPI.Model;
@@ -20,9 +21,19 @@ namespace ProAgil.WebAPI.Controllers
 
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<Evento>> Get()
+        public ActionResult Get()
         {
-           return _context.Eventos.ToList();
+            try
+            {
+              var results = _context.Eventos.ToList();
+             return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de dados falhou");
+            }
+          
         }
 
         // GET api/values/5
