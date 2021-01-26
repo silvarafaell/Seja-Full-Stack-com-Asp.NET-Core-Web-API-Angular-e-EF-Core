22abfa6cb98d9c962baff329a426cc9749d92f3b
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-01-25 23:35:48 -0300

    Aula 39 - Arquivos Estáticos

--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -48,6 +48,7 @@ namespace ProAgil.WebAPI
             }
 
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseStaticFiles();
             app.UseMvc();
         }
     }
