52ce4321ddc2fa1de8d2e0c06654129b84d0be3e
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-01-30 07:45:17 -0300

    Aula 65 - Modal

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Evento } from '../_models/Evento';
 import { EventoService } from '../_services/evento.service';
 
@@ -9,8 +10,21 @@ import { EventoService } from '../_services/evento.service';
 })
 export class EventosComponent implements OnInit {
 
+  eventosFiltrados!: Evento[];
+  eventos!: Evento[];
+  imagemLargura = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;
+  modalRef!: BsModalRef;
+
   // tslint:disable-next-line:variable-name
-  _filtroLista!: string;
+  _filtroLista = '';
+
+  constructor(
+      private eventoService: EventoService
+    , private modalService: BsModalService
+    ) { }
+
   get filtroLista(): string {
     return this._filtroLista;
   }
@@ -19,14 +33,10 @@ export class EventosComponent implements OnInit {
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
 
-  eventosFiltrados!: Evento[];
-  eventos!: Evento[];
-  imagemLargura = 50;
-  imagemMargem = 2;
-  mostrarImagem = false;
-
-  constructor(private eventoService: EventoService) { }
-
+  // tslint:disable-next-line:typedef
+  openModal(template: TemplateRef<any>){
+    this.modalRef = this.modalService.show(template);
+  }
   // tslint:disable-next-line:typedef
   ngOnInit() {
     this.getEventos();
